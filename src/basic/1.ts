8bb4c5276bfc8068b0f9f26b7700f978f5bbc08f
Andrey Isenko <aisenko87@gmail.com>
2025-03-14 21:04:24 +0200

    add hw

---
+++ src/basic/1.ts
@@ -0,0 +1,9 @@
+console.log(55);
+
+const age: number = 50;
+const username: string = "Max";
+const toggle: boolean = true;
+const empty: null = null;
+const callback = (a:number``) => {
+  return 100 + a;
+};
